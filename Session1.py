5699af0107495ccba2a2fd1dad06e4deedbeda53
taymourrajeh <taymourrajeh54@gmail.com>
2026-01-20 17:04:04 +0100

    this is my first commit

---
+++ Session1.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print(65+2)
+print("Are you selling any?")
+print("I have $", 100+200, ".", sep="!!")
+# end is used to tell print what to do after it prints.
+print("1", end="")
+print("2", end="")
+print("3", end="!!\n\n")
+
+
+
+
